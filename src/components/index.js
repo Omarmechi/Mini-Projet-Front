e6badf839fa7c0c13bbe092a80a5e450a2510898
Omarmechi <oumaralmeshy@gmail.com>
2024-03-16 02:26:35 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Register } from './register/Register.jsx'
+export { default as Home } from './home/Home.jsx'
+export { default as Navbar } from './navbar/Navbar.jsx'
+export { default as Dashboard } from './dashboard/Dashboard.jsx'
